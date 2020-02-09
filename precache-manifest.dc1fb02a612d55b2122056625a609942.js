47238d6e9c470158094b85f4bd3f1ee3ada0b74d
Aito Hideki <aito.hideki@gmail.com>
2020-02-09 06:48:43 -0800

    Updates

---
+++ precache-manifest.dc1fb02a612d55b2122056625a609942.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ce752342594bc9d870823d1ab71a5b87",
+    "url": "/milotest/index.html"
+  },
+  {
+    "revision": "4adda1820b1ab9e092d7",
+    "url": "/milotest/static/css/main.cce35fc8.chunk.css"
+  },
+  {
+    "revision": "fd478fa5e5a3ee48d3f1",
+    "url": "/milotest/static/js/2.02424d0e.chunk.js"
+  },
+  {
+    "revision": "4adda1820b1ab9e092d7",
+    "url": "/milotest/static/js/main.76279509.chunk.js"
+  },
+  {
+    "revision": "f576dc5a1d0787a7bf7a",
+    "url": "/milotest/static/js/runtime~main.20ebc9b9.js"
+  }
+]);
